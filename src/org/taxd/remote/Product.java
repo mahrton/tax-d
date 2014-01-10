b5388cb6dcfe25c387b644d2e7c7763c2225fc5b
Marton Tatai <marton.tatai@medicanimal.com>
2014-01-10 17:53:27 +0000

    missing pom file

--- src/org/taxd/remote/Product.java
+++ src/org/taxd/remote/Product.java
@@ -4,8 +4,17 @@ import java.io.Serializable;
 
 public class Product implements Serializable {
 	private static final long serialVersionUID = -4414015642821984680L;
-	private int netPriceCents = 0;
+	private long netPriceCents = 0;
 	private ProductType productType = ProductType.OTHER;
+	
+	public Product(ProductType productType) {
+		this.productType = productType;
+	}
+	
+	public Product(ProductType productType, long netPriceCents) {
+		this.netPriceCents = netPriceCents;
+		this.productType = productType;
+	}
 	
 	public enum ProductType {
 		FOOD(7),
