c12a858e12a215ff21d72b8584064f2e1606e4cb
mahrton <cethal@gmail.com>
2014-01-12 14:41:53 +0000

    deleted moved files

--- src/org/taxd/remote/Calculator.java
+++
@@ -1,9 +0,0 @@
-package org.taxd.remote;
-
-import java.util.List;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Calculator extends Remote {
-	double calcVat(List<Product> products) throws RemoteException;
-}
